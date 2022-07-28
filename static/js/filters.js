cab3d9f9d9e78fde94ecd61378f6cf41a59319c4
JackShen1 <jackshendrikov@gmail.com>
2022-07-28 23:57:15 +0300

    Run new linters & Update collection styles & Fix display of cast/movie detail

--- static/js/filters.js
+++ static/js/filters.js
@@ -1,187 +1,187 @@
-$(document).ready(function() {
-    getPlatforms();
-    getGenres();
-    getCountries();
-    getYears();
-    getAgeMarks();
-    getIMDbVotes();
-})
+$(document).ready(function () {
+  getPlatforms();
+  getGenres();
+  getCountries();
+  getYears();
+  getAgeMarks();
+  getIMDbVotes();
+});
 
 function getGenres() {
-    let url = $("#genres").attr("url");
+  let url = $("#genres").attr("url");
 
-    $.ajax({
-        method: 'GET',
-        url: url,
-        data: {},
-        success: function(result) {
-            let genres_option = "<li data-value=''>Any Genre</li>";
-            $.each(result["genres"], function(a, b) {
-                genres_option += `<li data-value='${b["categories__slug"]}'>${b["categories__name"]}</li>`
-            });
-            $("#genres").html(genres_option)
-        },
-        error: function(response) {
-            console.log(response)
-        },
-        async: false
-    });
+  $.ajax({
+    method: "GET",
+    url: url,
+    data: {},
+    success: function (result) {
+      let genres_option = "<li data-value=''>Any Genre</li>";
+      $.each(result.genres, function (a, b) {
+        genres_option += `<li data-value='${b.categories__slug}'>${b.categories__name}</li>`;
+      });
+      $("#genres").html(genres_option);
+    },
+    error: function (response) {
+      console.log(response);
+    },
+    async: false,
+  });
 }
 
 function getCountries() {
-    let url = $("#countries").attr("url");
+  let url = $("#countries").attr("url");
 
-    $.ajax({
-        method: 'GET',
-        url: url,
-        data: {},
-        success: function(result) {
-            let countries_option = "<li data-value=''>Any Country</li>";
-            $.each(result["countries"], function(a, b) {
-                countries_option += `<li data-value='${b["country__name"]}'>${b["country__name"]} (${b["country__code"]})</li>`
-            });
-            $("#countries").html(countries_option)
-        },
-        error: function(response) {
-            console.log(response)
-        },
-        async: false
-    });
+  $.ajax({
+    method: "GET",
+    url: url,
+    data: {},
+    success: function (result) {
+      let countries_option = "<li data-value=''>Any Country</li>";
+      $.each(result.countries, function (a, b) {
+        countries_option += `<li data-value='${b.country__name}'>${b.country__name} (${b.country__code})</li>`;
+      });
+      $("#countries").html(countries_option);
+    },
+    error: function (response) {
+      console.log(response);
+    },
+    async: false,
+  });
 }
 
 function getYears() {
-    let url = $("#years").attr("url");
+  let url = $("#years").attr("url");
 
-    $.ajax({
-        method: 'GET',
-        url: url,
-        data: {},
-        success: function(result) {
-            let yearSlider = document.getElementById('filter__years');
-            noUiSlider.create(yearSlider, {
-                range: {
-                    'min': result["years"][0],
-                    'max': result["years"][1]
-                },
-                step: 1,
-                connect: true,
-                start: [2000, new Date().getFullYear()],
-                format: {
-                    from: function(value) {
-                        return parseInt(value);
-                    },
-                    to: function(value) {
-                        return parseInt(value);
-                    }
-                }
-            });
-            let yearValues = [
-                document.getElementById('filter__years-start'),
-                document.getElementById('filter__years-end')
-            ];
-            let inputYearValues = [
-                document.getElementById('years-start'),
-                document.getElementById('years-end')
-            ];
-            yearSlider.noUiSlider.on('update', function(values, handle) {
-                yearValues[handle].innerHTML = values[handle];
-                inputYearValues[handle].setAttribute("value", values[handle]);
-            });
+  $.ajax({
+    method: "GET",
+    url: url,
+    data: {},
+    success: function (result) {
+      let yearSlider = document.getElementById("filter__years");
+      noUiSlider.create(yearSlider, {
+        range: {
+          min: result.years[0],
+          max: result.years[1],
         },
-        error: function(response) {
-            console.log(response)
+        step: 1,
+        connect: true,
+        start: [2000, new Date().getFullYear()],
+        format: {
+          from: function (value) {
+            return parseInt(value, 10);
+          },
+          to: function (value) {
+            return parseInt(value, 10);
+          },
         },
-        async: false
-    });
+      });
+      let yearValues = [
+        document.getElementById("filter__years-start"),
+        document.getElementById("filter__years-end"),
+      ];
+      let inputYearValues = [
+        document.getElementById("years-start"),
+        document.getElementById("years-end"),
+      ];
+      yearSlider.noUiSlider.on("update", function (values, handle) {
+        yearValues[handle].innerHTML = values[handle];
+        inputYearValues[handle].setAttribute("value", values[handle]);
+      });
+    },
+    error: function (response) {
+      console.log(response);
+    },
+    async: false,
+  });
 }
 
 function getIMDbVotes() {
-    let url = $("#imdb_votes").attr("url");
-    if (url) {
-        $.ajax({
-            method: 'GET',
-            url: url,
-            data: {},
-            success: function (result) {
-                let votesSlider = document.getElementById('filter__votes');
-                noUiSlider.create(votesSlider, {
-                    range: {
-                        'min': 0,
-                        'max': result["imdb_votes"]
-                    },
-                    step: 10000,
-                    connect: true,
-                    start: [30000, result["imdb_votes"]],
-                    format: {
-                        from: function (value) {
-                            return parseInt(value);
-                        },
-                        to: function (value) {
-                            return parseInt(value);
-                        }
-                    }
-                });
-                let votesValues = [
-                    document.getElementById('filter__votes-start'),
-                    document.getElementById('filter__votes-end')
-                ];
-                let inputVotesValues = [
-                    document.getElementById('imdb_votes-start'),
-                    document.getElementById('imdb_votes-end')
-                ];
-                votesSlider.noUiSlider.on('update', function (values, handle) {
-                    votesValues[handle].innerHTML = values[handle];
-                    inputVotesValues[handle].setAttribute("value", values[handle]);
-                });
+  let url = $("#imdb_votes").attr("url");
+  if (url) {
+    $.ajax({
+      method: "GET",
+      url: url,
+      data: {},
+      success: function (result) {
+        let votesSlider = document.getElementById("filter__votes");
+        noUiSlider.create(votesSlider, {
+          range: {
+            min: 0,
+            max: result.imdb_votes,
+          },
+          step: 10000,
+          connect: true,
+          start: [30000, result.imdb_votes],
+          format: {
+            from: function (value) {
+              return parseInt(value, 10);
             },
-            error: function (response) {
-                console.log(response)
+            to: function (value) {
+              return parseInt(value, 10);
             },
-            async: false
+          },
         });
-    }
+        let votesValues = [
+          document.getElementById("filter__votes-start"),
+          document.getElementById("filter__votes-end"),
+        ];
+        let inputVotesValues = [
+          document.getElementById("imdb_votes-start"),
+          document.getElementById("imdb_votes-end"),
+        ];
+        votesSlider.noUiSlider.on("update", function (values, handle) {
+          votesValues[handle].innerHTML = values[handle];
+          inputVotesValues[handle].setAttribute("value", values[handle]);
+        });
+      },
+      error: function (response) {
+        console.log(response);
+      },
+      async: false,
+    });
+  }
 }
 
 function getAgeMarks() {
-    let url = $("#age_marks").attr("url");
+  let url = $("#age_marks").attr("url");
 
-    if (url) {
-        $.ajax({
-            method: 'GET',
-            url: url,
-            data: {},
-            success: function (result) {
-                let platforms_option = "<li data-value=''>Any</li>";
-                $.each(result["age_marks"], function (a, b) {
-                    platforms_option += `<li data-value='${b["age_mark"]}'>${b["age_mark"]}</li>`
-                });
-                $("#age_marks").html(platforms_option)
-            },
-            error: function (response) {
-                console.log(response)
-            },
-            async: false
+  if (url) {
+    $.ajax({
+      method: "GET",
+      url: url,
+      data: {},
+      success: function (result) {
+        let platforms_option = "<li data-value=''>Any</li>";
+        $.each(result.age_marks, function (a, b) {
+          platforms_option += `<li data-value='${b.age_mark}'>${b.age_mark}</li>`;
         });
-    }
+        $("#age_marks").html(platforms_option);
+      },
+      error: function (response) {
+        console.log(response);
+      },
+      async: false,
+    });
+  }
 }
 
 function getPlatforms() {
-    let url = $("#platforms").attr("url");
+  let url = $("#platforms").attr("url");
 
-    $.ajax({
-        method: 'GET',
-        url: url,
-        data: {},
-        success: function(result) {
-            let platforms_option = "<li data-value=''>Any Platform</li>";
-            $.each(result["platforms"], function(a, b) {
-                platforms_option += `<li data-value='${b["service"]}'>${b["service"]}</li>`
-            });
-            $("#platforms").html(platforms_option)
-        },
-        error: function(response) {
-            console.log(response)
-        },
-        async: false
-    });
+  $.ajax({
+    method: "GET",
+    url: url,
+    data: {},
+    success: function (result) {
+      let platforms_option = "<li data-value=''>Any Platform</li>";
+      $.each(result.platforms, function (a, b) {
+        platforms_option += `<li data-value='${b.service}'>${b.service}</li>`;
+      });
+      $("#platforms").html(platforms_option);
+    },
+    error: function (response) {
+      console.log(response);
+    },
+    async: false,
+  });
 }
