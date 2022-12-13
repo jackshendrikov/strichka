d114380e4a27522cee669d84f9499b492b485f0a
JackShen1 <jackshendrikov@gmail.com>
2022-12-13 20:58:26 +0200

    Add documentation

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,5 @@
 import os
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
